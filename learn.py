ada09d9ec5abb0a30e5de19cad83b7328cdff696
WORK\Yaroslav <yaroslavdementev2010@gmail.com>
2020-07-17 18:48:28 +0300

    Переменные, типы данных, вычисения, строки.

--- learn.py
+++ learn.py
@@ -11,3 +11,44 @@ right(90)
 forward(100)
 right(90)
 forward(100)
+print('Я нарисовал квадрат!')
+
+# Переменные
+run = 5
+print(run)
+
+name = 'Yaroslav'
+print(name)
+
+run = 10
+print(run)
+
+name = run
+print(run, name)
+
+run = 7
+print(run, name)
+
+# Типы данных
+# apple = input('Enter number of apples')
+# print(apple + '1') #Текст+текст
+
+# apple1 = input('Enter number of apples')
+# print(int(apple1) + 1) #Число+число
+
+#Вычмсления
+ants = 5
+spiders = 70
+bug = ants + spiders
+print(bug)
+ants = 100
+spiders = 100
+print(bug)
+
+#Строки
+a = 'run'
+b = ' Пришельцы прилетели!'
+c = a + b + ' war'
+print(c)
+
+len(c)
