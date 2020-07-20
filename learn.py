3f3b44e78c2c158a530b3bc6c63dfc2d1f28087c
WORK\Yaroslav <yaroslavdementev2010@gmail.com>
2020-07-20 21:04:35 +0300

    Ввод и вывод. If Else.

--- learn.py
+++ learn.py
@@ -1,54 +1,75 @@
-a = 10
-b = 5
-print(a + b)
-print(a - b)
+# a = 10
+# b = 5
+# print(a + b)
+# print(a - b)
 
-from turtle import *
-forward(100)
-right(90)
-forward(100)
-right(90)
-forward(100)
-right(90)
-forward(100)
-print('Я нарисовал квадрат!')
+# from turtle import *
+# forward(100)
+# right(90)
+# forward(100)
+# right(90)
+# forward(100)
+# right(90)
+# forward(100)
+# print('Я нарисовал квадрат!')
 
-# Переменные
-run = 5
-print(run)
+# # Переменные
+# run = 5
+# print(run)
 
-name = 'Yaroslav'
-print(name)
+# name = 'Yaroslav'
+# print(name)
 
-run = 10
-print(run)
+# run = 10
+# print(run)
 
-name = run
-print(run, name)
+# name = run
+# print(run, name)
 
-run = 7
-print(run, name)
+# run = 7
+# print(run, name)
 
-# Типы данных
-# apple = input('Enter number of apples')
-# print(apple + '1') #Текст+текст
+# # Типы данных
+# # apple = input('Enter number of apples')
+# # print(apple + '1') #Текст+текст
 
-# apple1 = input('Enter number of apples')
-# print(int(apple1) + 1) #Число+число
+# # apple1 = input('Enter number of apples')
+# # print(int(apple1) + 1) #Число+число
 
-#Вычмсления
-ants = 5
-spiders = 70
-bug = ants + spiders
-print(bug)
-ants = 100
-spiders = 100
-print(bug)
+# #Вычмсления
+# ants = 5
+# spiders = 70
+# bug = ants + spiders
+# print(bug)
+# ants = 100
+# spiders = 100
+# print(bug)
 
-#Строки
-a = 'run'
-b = ' Пришельцы прилетели!'
-c = a + b + ' war'
-print(c)
+# #Строки
+# a = 'run'
+# b = ' Пришельцы прилетели!'
+# c = a + b + ' war'
+# print(c)
+# len(c)
 
-len(c)
+# #Нумерация символов
+# d = 'Harry Potter'
+# d[4]
+# d[6:12]
+# d[:5]
+# d[6:]
+
+# print('Привет!\'Как\' дела?') #\ - экранирование.
+
+# #Ввод и вывод
+# name = input('Как тебя зовут?')
+# print('Привет,', name, sep=('\n'))
+
+# old = input('Сколько тебе лет?')
+# print('Мне,',old,'лет', sep=('-')) 
+
+#Принятие решений
+toys = 10
+toys == 1
+
+#Ветвление
